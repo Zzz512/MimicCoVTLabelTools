73c4641533c8b67697b01100c6f96d072ae20e32
Zzz <wxy@ZzzdeMacBook-Pro.local>
2024-04-22 16:30:10 +0800

    conflict solved

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ def main():
         sys.exit(0)
 
     setup(
-        name="labelme",
+        name="labelme-covt",
         version=version,
         packages=find_packages(),
         description="Image Polygonal Annotation with Python",
@@ -145,7 +145,7 @@ def main():
         package_data={"labelme": ["icons/*", "config/*.yaml", "translate/*"]},
         entry_points={
             "console_scripts": [
-                "labelme=labelme.__main__:main",
+                "labelme-covt=labelme.__main__:main",
                 "labelme_draw_json=labelme.cli.draw_json:main",
                 "labelme_draw_label_png=labelme.cli.draw_label_png:main",
                 "labelme_json_to_dataset=labelme.cli.json_to_dataset:main",
